8fa0fdeb9352c584357482f07435c622124c9460
Dorukhan User <userdorukhan@berkeley.edu>
2026-04-01 17:59:53 -0700

    Small change

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -66,17 +66,17 @@ void Error_Handler(void);
 #define SWCLK_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
-#define AD9851_W_CLK_Pin GPIO_PIN_2
-#define AD9851_W_CLK_GPIO_Port GPIOE
+#define AD9851_W_CLK_Pin GPIO_PIN_3
+#define AD9851_W_CLK_GPIO_Port GPIOB
 
-#define AD9851_FQ_UD_Pin GPIO_PIN_3
-#define AD9851_FQ_UD_GPIO_Port GPIOE
+#define AD9851_FQ_UD_Pin GPIO_PIN_0
+#define AD9851_FQ_UD_GPIO_Port GPIOC
 
-#define AD9851_DATA_Pin GPIO_PIN_4
-#define AD9851_DATA_GPIO_Port GPIOE
+#define AD9851_DATA_Pin GPIO_PIN_10
+#define AD9851_DATA_GPIO_Port GPIOB
 
-#define AD9851_RESET_Pin GPIO_PIN_5
-#define AD9851_RESET_GPIO_Port GPIOE
+#define AD9851_RESET_Pin GPIO_PIN_1
+#define AD9851_RESET_GPIO_Port GPIOC
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
--- Core/Src/main.c
+++ Core/Src/main.c
@@ -218,19 +218,27 @@ static void MX_GPIO_Init(void)
   /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOC_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_3|GPIO_PIN_10, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_0|GPIO_PIN_1, GPIO_PIN_RESET);
 
-  /*Configure GPIO pins : PE2 PE3 PE4 PE5 */
-  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3|GPIO_PIN_4|GPIO_PIN_5;
+  /*Configure GPIO pins : PB3 PB10 */
+  GPIO_InitStruct.Pin = GPIO_PIN_3|GPIO_PIN_10;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PC0 PC1 */
+  GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
